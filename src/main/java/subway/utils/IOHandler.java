2f4fec9c3486135dcd09e0b24eeffa1240a50024
Lee-Jungyu <jungyu09@gmail.com>
2020-12-11 01:30:38 +0900

    feat: Add menu print

--- src/main/java/subway/utils/IOHandler.java
+++ src/main/java/subway/utils/IOHandler.java
@@ -12,6 +12,73 @@ public class IOHandler {
         this.scanner = scanner;
     }
 
+    public String printMainMenu() {
+        System.out.println("## 메인 화면");
+        System.out.println("1. 역 관리");
+        System.out.println("2. 노선 관리");
+        System.out.println("3. 구간 관리");
+        System.out.println("4. 지하철 노선도 출력");
+        System.out.println("Q. 돌아가기");
+        System.out.println();
+
+        String menu = scanner.next();
+        return menu;
+    }
+
+    public String printStationMenu() {
+        System.out.println("## 역 관리 화면");
+        System.out.println("1. 역 등록");
+        System.out.println("2. 역 삭제");
+        System.out.println("3. 역 조회");
+        System.out.println("B. 돌아가기");
+        System.out.println();
+
+        String menu = scanner.next();
+        return menu;
+    }
+
+    public String printLineMenu() {
+        System.out.println("## 노선 관리 화면");
+        System.out.println("1. 노선 등록");
+        System.out.println("2. 노선 삭제");
+        System.out.println("3. 노선 조회");
+        System.out.println("B. 돌아가기");
+        System.out.println();
+
+        String menu = scanner.next();
+        return menu;
+    }
+
+    public String printSectionMenu() {
+        System.out.println("## 구간 관리 화면");
+        System.out.println("1. 구간 등록");
+        System.out.println("2. 구간 삭제");
+        System.out.println("B. 돌아가기");
+        System.out.println();
+
+        String menu = scanner.next();
+        return menu;
+    }
+    
+    public void printStationsInLines() {
+        System.out.println("## 지하철 노선도");
+
+        for(Line line : LineRepository.lines()) {
+            String lineName = line.getName();
+
+            printInfo(lineName);
+            printInfo("---");
+
+            List<Station> stations = SectionRepository.findStationsByLineName(lineName);
+
+            for(Station station : stations)
+                printInfo(station.getName());
+
+            printInfo("---");
+            System.out.println();
+        }
+    }
+
     public void inputStation() {
         String stationName;
 
@@ -198,25 +265,6 @@ public class IOHandler {
             printInfo("구간이 삭제되었습니다.");
         } catch (IllegalArgumentException e) {
             printError(e.getMessage());
-        }
-    }
-
-    public void printStationsInLines() {
-        System.out.println("## 지하철 노선도");
-
-        for(Line line : LineRepository.lines()) {
-            String lineName = line.getName();
-
-            printInfo(lineName);
-            printInfo("---");
-
-            List<Station> stations = SectionRepository.findStationsByLineName(lineName);
-
-            for(Station station : stations)
-                printInfo(station.getName());
-
-            printInfo("---");
-            System.out.println();
         }
     }
 
